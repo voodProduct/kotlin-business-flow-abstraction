4c7160feb3af52a44de45650da5d7682ebdf14ca
vood <igor-vood@gmail.com>
2025-03-23 13:25:12 +0300

    init

---
+++ example-app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "ru.vood.flow"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
-rootProject.name = "abstraction"
+pluginManagement {
+
+
+    plugins {
+        kotlin("jvm") version "2.1.0"
+        kotlin("plugin.spring") version "2.1.0"
+        id("org.springframework.boot") version "3.4.4"
+        id("io.spring.dependency-management") version "1.1.7"
+    }
+
+    repositories {
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+include(":example-app")
