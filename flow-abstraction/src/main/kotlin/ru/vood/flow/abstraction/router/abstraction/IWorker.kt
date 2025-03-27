bfb8f1c56b59a41f9077bc2cb76f3369198bee7b
vood <igor-vood@gmail.com>
2025-03-27 13:53:33 +0300

    init

--- flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/IWorker.kt
+++ flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/IWorker.kt
@@ -1,6 +1,8 @@
 package ru.vood.flow.abstraction.router.abstraction
 
-interface IWorker<out rId: IWorkerId> {
+interface IWorker<
+        out rId: IWorkerId
+        > {
     val workerId: rId
     suspend fun<T, R> doWork(data: T): R
 }
