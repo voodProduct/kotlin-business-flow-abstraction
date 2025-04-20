c6b6273a38426606c3b81ca2c770737027bc5bfa
vood <igor-vood@gmail.com>
2025-04-20 14:15:17 +0300

    init

--- flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/byInClass/AbstractRouterByInOutClass.kt
+++ flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/byInClass/AbstractRouterByInOutClass.kt
@@ -2,6 +2,16 @@ package ru.vood.flow.abstraction.router.abstraction.byInClass
 
 import ru.vood.flow.abstraction.router.abstraction.AbstractRouter
 
+/**
+ * Абстрактный класс роутера, расширяющий базовый класс [AbstractRouter], предназначенный для удобной маршрутизации данных по идентификаторам рабочих объектов.
+ *
+ * Данный класс предоставляет дополнительную функциональность поверх базовой маршрутизации, позволяя гибко управлять обработкой данных в зависимости от конкретного рабочего объекта.
+ *
+ * @param T Общий тип входных данных, которые будут переданы рабочим объектам.
+ * @param R Общий тип результата, возвращаемый рабочими объектами.
+ * @param wId Тип идентификатора рабочего объекта, реализуемого интерфейсом [IWorkerIdByInClass].
+ * @param worker Тип рабочего объекта, реализованного интерфейсом [IWorkerByInClass].
+ */
 abstract class AbstractRouterByInClass<
         T : Any,
         R : Any,
@@ -11,12 +21,25 @@ abstract class AbstractRouterByInClass<
     iWorkerList: List<worker>
 ) : AbstractRouter<T, R, wId, worker>(iWorkerList) {
 
+    /**
+     * Подготавливает и отправляет данные нужному рабочему объекту, основываясь на его идентификаторе.
+     *
+     * @param IT Реализованный тип входных данных.
+     * @param IR Реализованный тип возвращаемых результатов.
+     * @param data Лямбда-выражение, которое получает входные данные.
+     * @param idF Лямбда-выражение, возвращающее идентификатор рабочего объекта.
+     * @return Результат работы рабочего объекта.
+     * @throws RuntimeException Если рабочий объект с указанным идентификатором не найден.
+     */
     suspend inline fun <reified IT : T, reified IR : R> mapData(
         crossinline data: suspend () -> IT,
         idF: () -> wId
     ): IR {
+        // Получаем идентификатор рабочего объекта
         val validateMapperId = idF()
+        // Получаем входные данные
         return data().let { dataDto ->
+            // Выполняем работу нужным рабочим объектом
             routedMap[validateMapperId]?.doWork(dataDto, validateMapperId) as IR?
         } ?: error("For router ${this::class.java.canonicalName} not found worker with Id $validateMapperId")
     }
--- flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/byInClass/IWorkerByInClass.kt
+++ flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/byInClass/IWorkerByInClass.kt
@@ -2,7 +2,19 @@ package ru.vood.flow.abstraction.router.abstraction.byInClass
 
 import ru.vood.flow.abstraction.router.abstraction.IWorker
 
+/**
+ * Интерфейс рабочего объекта, предназначенного для обработки данных определенного типа и возвращения результата другого типа.
+ * Отличительной особенностью является зависимость от специализированного идентификатора [IWorkerIdByInClass],
+ * что позволяет применять дополнительные правила проверки и фильтрации данных.
+ *
+ * @param T Тип входных данных, принимаемых рабочим объектом.
+ * @param R Тип результата, возвращаемого рабочим объектом.
+ * @param WORKER_ID Специфический тип идентификатора рабочего объекта, унаследованный от [IWorkerIdByInClass].
+ */
 interface IWorkerByInClass<
+        /** Тип входных данных*/
         T : Any,
+        /** Тип возвращаемых данных*/
         out R : Any,
+        /**Специфичный тип идентификатора*/
          WORKER_ID : IWorkerIdByInClass<T>> : IWorker<T, R, WORKER_ID>
