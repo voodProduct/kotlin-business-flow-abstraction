2e51eb9324b6ad48d38a48d24b0b859bec6a12a3
vood <igor-vood@gmail.com>
2025-04-25 16:15:04 +0300

    init

--- flow-abstraction/src/main/kotlin/ru/vood/flow/abstraction/router/abstraction/enumR/IEnumWorkerId.kt
+++
@@ -1,7 +0,0 @@
-package ru.vood.flow.abstraction.router.abstraction.enumR
-
-import ru.vood.flow.abstraction.router.abstraction.IWorkerId
-
-interface IEnumWorkerId<E : Enum<E>> : IWorkerId {
-    fun enumValue(): E
-}
