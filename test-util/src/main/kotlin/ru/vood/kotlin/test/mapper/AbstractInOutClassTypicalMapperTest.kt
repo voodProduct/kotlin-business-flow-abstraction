63e8dd153b64bc0dd70dc27933033dfc45390317
vood <igor-vood@gmail.com>
2025-12-17 18:39:24 +0300

    mapper test

--- test-util/src/main/kotlin/ru/vood/kotlin/test/mapper/AbstractInOutClassTypicalMapperTest.kt
+++ test-util/src/main/kotlin/ru/vood/kotlin/test/mapper/AbstractInOutClassTypicalMapperTest.kt
@@ -3,7 +3,6 @@ package ru.vood.kotlin.test.mapper
 import com.ocadotechnology.gembus.test.Arranger
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.datatest.WithDataTestName
 import io.kotest.datatest.withData
 import io.kotest.matchers.shouldBe
 import ru.vood.flow.abstraction.router.abstraction.byInOutClass.IWorkerByInOutClass
@@ -13,41 +12,59 @@ import ru.vood.flow.abstraction.router.abstraction.byInOutClass.IWorkerIdByInOut
  *  T - класс из которого мапятся данные
  *  R - класс, в который будут записаны данные
  * */
-abstract class AbstractInOutClassTypicalMapperTest<T : Any, R : Any, WORKER_ID: IWorkerIdByInOutClass<T, R>> : FunSpec() {
+abstract class AbstractInOutClassTypicalMapperTest<T : Any, R : Any, WORKER_ID : IWorkerIdByInOutClass<T, R>> :
+    FunSpec() {
 
 
     /** экземпляр маппера, который сейчас тестируется */
     abstract val iMapper: IWorkerByInOutClass<T, R, WORKER_ID>
 
     /** экземпляр маппера, который сейчас тестируется */
-    abstract val testedWorkerId: WORKER_ID
+    abstract val workerId: WORKER_ID
 
     /**перечень тест кейзов */
-    abstract val testCases: List<TestCaseData<T, R>>
+    abstract val testCases: List<ITestCase<T, R>/*TestCaseData<T, R>*/>
 
     /**генерирует экземпляр ДТО с тестовыми данными, запоминает первый экземпляр */
     val testDataEtalon: T by lazy {
-        genTestDataEtalon()
+        generateTestDataEtalon()
     }
 
     /** генерирует экземпляр ДТО с тестовыми данными, при каждом запуске генерируется новый ДТО
      * */
-    protected open fun genTestDataEtalon(): T = Arranger.some(testedWorkerId.tkClass.java)
+    protected open fun generateTestDataEtalon(): T = Arranger.some(workerId.tkClass.java)
 
     init {
         withData(this.testCases) {
             with(it) {
                 mockks()
-                if (error == null) {
-                    val mapData: R = iMapper.doWork(testCaseTestData, testedWorkerId)
-                    val extractedVal: Any? = extractFun(mapData)
-                    extractedVal shouldBe expectedVal
-                } else {
-                    val shouldThrow = shouldThrow<IllegalStateException> {
-                        iMapper.doWork(testCaseTestData, testedWorkerId)
+                when (this) {
+                    is ErrorTestCaseData<T, R> -> {
+                        val shouldThrow = shouldThrow<IllegalStateException> {
+                            iMapper.doWork(testCaseTestData, workerId)
+                        }
+                        shouldThrow.message shouldBe error
                     }
-                    shouldThrow.message shouldBe error
+                    is OkTestCaseData<T, R> -> {
+                        val mapData: R = iMapper.doWork(testCaseTestData, workerId)
+                        val extractedVal: Any? = extractFun(mapData)
+                        extractedVal shouldBe expectedVal
+                    }
+                    is TestCaseData<T, R> -> {
+                        if (error == null) {
+                            val mapData: R = iMapper.doWork(testCaseTestData, workerId)
+                            val extractedVal: Any? = extractFun(mapData)
+                            extractedVal shouldBe expectedVal
+                        } else {
+                            val shouldThrow = shouldThrow<IllegalStateException> {
+                                iMapper.doWork(testCaseTestData, workerId)
+                            }
+                            shouldThrow.message shouldBe error
+                        }
+                    }
                 }
+
+
             }
         }
     }
--- test-util/src/main/kotlin/ru/vood/kotlin/test/mapper/ITestCase.kt
+++ test-util/src/main/kotlin/ru/vood/kotlin/test/mapper/ITestCase.kt
@@ -2,62 +2,62 @@ package ru.vood.kotlin.test.mapper
 
 import io.kotest.datatest.WithDataTestName
 
-sealed interface ITestCase<TT : Any> : WithDataTestName {
+sealed interface ITestCase<T : Any, R : Any> : WithDataTestName {
     /**словесное описание тест кейза, будет выведено при прогоне*/
     val description: String
 
     /**входная ДТО для маппера*/
-    val testCaseTestData: TT
+    val testCaseTestData: T
 
     /**набор моков, если нужен */
     val mockks: () -> Any?
     override fun dataTestName(): String = description
 }
 
-data class ErrorTestCaseData<TT : Any>(
+data class ErrorTestCaseData<T : Any, R : Any>(
     /**словесное описание тест кейза, будет выведено при прогоне*/
     override val description: String,
     /**входная ДТО для маппера*/
-    override val testCaseTestData: TT,
+    override val testCaseTestData: T,
     /**если планируется что при маппинге должено быть исключение, то тут должен быть записан его текст,
      * заполняется, то лько если extractFun и expectedVal пустые
      * */
     val error: String,
     /**набор моков, если нужен */
     override val mockks: () -> Any? = {},
-) : ITestCase<TT>
+) : ITestCase<T, R>
 
-data class OkTestCaseData<TT : Any, RR : Any>(
+data class OkTestCaseData<T : Any, R : Any>(
     /**словесное описание тест кейза, будет выведено при прогоне*/
     override val description: String,
     /**входная ДТО для маппера*/
-    override val testCaseTestData: TT,
+    override val testCaseTestData: T,
     /**ф-ция, которая принимает ДТО с выхода маппера и может вытащить конкретный атрибут.*/
-    val extractFun: (RR) -> Any? = { error("override extractFun") },
+    val extractFun: (R) -> Any? = { error("override extractFun") },
     /**ожидаемый результат, будет сравнен с тем что вернула extractFun */
     val expectedVal: Any? = null,
     /**набор моков, если нужен */
     override val mockks: () -> Any? = {},
-) : ITestCase<TT>
+) : ITestCase<T, R>
 
 
 /** тесткейз */
-data class TestCaseData<TT : Any, RR : Any>(
+data class TestCaseData<T : Any, R : Any>(
     /**словесное описание тест кейза, будет выведено при прогоне*/
     override val description: String,
     /**входная ДТО для маппера*/
-    override val testCaseTestData: TT,
+    override val testCaseTestData: T,
     /**если планируется что при маппинге должено быть исключение, то тут должен быть записан его текст,
      * заполняется, то лько если extractFun и expectedVal пустые
      * */
     val error: String? = null,
     /**ф-ция, которая принимает ДТО с выхода маппера и может вытащить конкретный атрибут.*/
-    val extractFun: (RR) -> Any? = { error("override extractFun") },
+    val extractFun: (R) -> Any? = { error("override extractFun") },
     /**ожидаемый результат, будет сравнен с тем что вернула extractFun */
     val expectedVal: Any? = null,
     /**набор моков, если нужен */
     override val mockks: () -> Any? = {}
-) : WithDataTestName, ITestCase<TT> {
+) : WithDataTestName, ITestCase<T, R> {
     init {
         val notErrorFl = error == null
         val errorFl = error != null && expectedVal == null
